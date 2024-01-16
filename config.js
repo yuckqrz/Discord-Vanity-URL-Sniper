aa2bd0ad9ff96b3519b7fa225e9e675a1e7401f7
eric <81445655+yuckqrz@users.noreply.github.com>
2024-01-15 20:21:59 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 const config = {
-  TOKEN: "", //write your discord bot token
-  url: "", //Type the Custom Discord Server URL You Want to Retrieve!
-  serverID: "", //Type the Server ID to Get the Url!
+  TOKEN: "MTE2Mzk4NDI3NjA5ODkyMDUzOQ.GnGP51.Xd0RJpIBRcrSfUkF6DKoWYjGy_ncOLTEnbSz9E", //write your discord bot token
+  url: "discord.gg/kcord", //Type the Custom Discord Server URL You Want to Retrieve!
+  serverID: "1188570077004574760", //Type the Server ID to Get the Url!
 
   auth:{
-    discordAccountToken: "" //Type a discord account token that is authorised on the discord server where you will receive the url.
+    discordAccountToken: "NjA3NTg0NDY0OTU2MDMwOTc2.GwKP-O.CTgGRKn6ITO5-ySPHbJJlebbFcy364CdEhlAis" //Type a discord account token that is authorised on the discord server where you will receive the url.
   }
 }
 
